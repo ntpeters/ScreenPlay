0111792b9fce56e48e932db57c168949850f4bfb
ejrinkus <ejrinkus@mtu.edu>
2014-12-07 01:17:29 -0600

    Castelvania demo

--- ScreenPlay/app.ts
+++ ScreenPlay/app.ts
@@ -1,24 +1,5 @@
-﻿class SimpleGame {
+﻿window.onload = () => {
 
-    constructor() {
-        this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', { preload: this.preload, create: this.create });
-    }
-
-    game: Phaser.Game;
-
-    preload() {
-        this.game.load.image('logo', 'phaser-logo-small.png');
-    }
-
-    create() {
-        var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
-        logo.anchor.setTo(0.5, 0.5);
-    }
-
-}
-
-window.onload = () => {
-
-    var game = new SimpleGame();
+    var game = new Castlevania.Game();
 
 };
